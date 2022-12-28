0d205a6b832dd946494c62af24afdf34c64dd257
Abel Ngbandaman <ngbandaman2022@gmail.com>
2022-12-28 21:37:13 +0000

    Week2Day2: Exercice 1 done

---
+++ Week2/Day2/ExerciceXP/script.js
@@ -0,0 +1,14 @@
+// Exercice 1
+    // Question 1 : creation of variables x and y
+    let x=2, y= 5;
+
+    // Question 2 : checking which number is bigger
+    if(x<y){
+        console.log(`${y} is biggest number`); // Result to display
+    }else{
+        console.log(`${x} is biggest number`); // Result to display
+    }
+
+// Exercice 2
+    // Question 1 : creation of newDog variable
+    let newDog = "Chihuahua";
