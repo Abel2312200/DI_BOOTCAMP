e8de83083631075dbc6ebef7bc278974b5d2f7ba
Abel Ngbandaman <ngbandaman2022@gmail.com>
2022-12-28 21:51:43 +0000

    Week2_Day2_ExerciceXP: Exercice 2 done.

--- Week2/Day2/ExerciceXP/script.js
+++ Week2/Day2/ExerciceXP/script.js
@@ -12,3 +12,17 @@
 // Exercice 2
     // Question 1 : creation of newDog variable
     let newDog = "Chihuahua";
+
+    // Question 2 : letters in newDog variable
+    let numberLetterNewDog = newDog.length;
+    console.log(`${newDog} has ${numberLetterNewDog} letters.`);
+
+    // Question 3 : displaying newDog in uppercase
+    console.log(`${newDog} in uppercase give out ${newDog.toUpperCase}`);
+
+    // Question 4 : checking newDog is equal to "Chihuahua"
+    if(newDog === "Chihuahua"){
+        console.log( "I love Chihuahuas, it’s my favorite dog breed");
+    }else{
+        console.log("I don't car, I prefer cats");
+    }
