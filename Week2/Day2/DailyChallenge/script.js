79ab42b527b2476ed63860d11b78f453e304c4f0
Abel Ngbandaman <ngbandaman2022@gmail.com>
2022-12-28 23:45:47 +0000

    Week2_Day2_DailyChallenge: Question 3 and 4  done.

--- Week2/Day2/DailyChallenge/script.js
+++ Week2/Day2/DailyChallenge/script.js
@@ -1,7 +1,19 @@
 // Question 1
-    let sentence = ["not","bad"];
+    let sentence = "The movie is not that bad for me, I like it";
+    let split_sentence = sentence.split(" "); //split_sentence = [ "The","movie","is","not","that","bad","I","like","it"]
 
 // Question 2
-    let wordNot = sentence[0];
+    let indexNot = split_sentence.indexOf('not') // researching index of "not" in split_sentece array
+    let wordNot = split_sentence[indexNot];     
+    
+// Question 3
+    let indexBad = split_sentence.indexOf('bad') // researching index of "bad" in split_sentece array
+    let wordBad = split_sentence[indexBad];
 
-// 
+// Question 4
+    if(indexNot<indexBad){
+        sentence = sentence.replace('not that bad','good'); // replace 'not that bad' by 'good'
+        console.log(sentence);
+    } else{
+        console.log(sentence);
+    }
