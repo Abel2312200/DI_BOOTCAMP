b34bb18a91b987a219c87d4622bba84df246ab5f
Abel Ngbandaman <ngbandaman2022@gmail.com>
2023-01-05 01:16:30 +0000

    Week3_Day1_ExerciceXp: exercice 3 done

--- Week3/Day1/ExerciceXP/Exercice3/script.js
+++ Week3/Day1/ExerciceXP/Exercice3/script.js
@@ -1,7 +1,7 @@
-// Question 2
+// Question 2 : changing the value of navBar's id
 document.querySelector('div').setAttribute("id","socialNetworkNavigation");
 
-// Question 3
+// Question 3 : adding a new li
 let newIl = document.createElement('li')  
 
 let newTextNode = document.createTextNode('Logout');
@@ -12,9 +12,11 @@ document.querySelector('ul').appendChild(newIl);
 
 // Bonus
 
+// retrieving first and last li
 let firstIl = document.querySelector('ul').firstElementChild;
 let lastIl = document.querySelector('ul').lastElementChild;
 
+// displaying content first and last li
 console.log(`${firstIl.children[0].textContent}`);
 console.log(`${lastIl.textContent}`);
 
