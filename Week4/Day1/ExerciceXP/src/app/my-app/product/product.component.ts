90dc7ef719eb0d982cd252ef6a9b62efc47c400e
Abel Ngbandaman <ngbandaman2022@gmail.com>
2023-01-10 02:38:51 +0000

    Week4_Day1_ExerciceXP: initialization ExerciceXP done

---
+++ Week4/Day1/ExerciceXP/src/app/my-app/product/product.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent implements OnInit {
+
+  public product : object = {
+    name : 'Orange',
+    price : 350
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
