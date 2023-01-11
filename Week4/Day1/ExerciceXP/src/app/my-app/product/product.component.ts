e076ad904edd0abb7cbdd9c56f031f9125ec4ab6
Abel Ngbandaman <ngbandaman2022@gmail.com>
2023-01-11 07:26:33 +0000

    Week2_Day4 : daily challenge done

--- Week4/Day1/ExerciceXP/src/app/my-app/product/product.component.ts
+++ Week4/Day1/ExerciceXP/src/app/my-app/product/product.component.ts
@@ -5,6 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
+
 export class ProductComponent implements OnInit {
 
   public product : object = {
